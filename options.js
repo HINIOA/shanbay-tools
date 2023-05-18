32e9fff94e686d790caf98c1e0548a1a0d726bc7
Qiu Xinyao <xinyao.qiu@shopee.com>
2023-05-18 11:50:42 +0800

    fix(option): 移除多余代码

--- options.js
+++ options.js
@@ -1,9 +1,11 @@
 const options = ["isSelectOn"];
 
 function restoreOptions() {
-  chrome.storage.sync.get(options, items => {
-    options.forEach(option => document.getElementById(option).checked = items[option])
-  })
+  chrome.storage.sync.get(options, (items) =>
+    options.forEach(
+      (option) => (document.getElementById(option).checked = items[option])
+    )
+  );
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
@@ -11,7 +13,6 @@ document.addEventListener("DOMContentLoaded", restoreOptions);
 options.forEach((option) =>
   document.getElementById(option).addEventListener("change", () => {
     const checked = document.getElementById(option).checked;
-    document.getElementById("result").innerHTML = `${option} ${checked}`;
     chrome.storage.sync.set({ [option]: checked });
   })
 );
